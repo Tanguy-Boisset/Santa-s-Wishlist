06bc642147d441a87396348f9920d8b2d26a0042
Tanguy Boisset <tanguy.boisset@gmail.com>
2022-03-21 13:41:25 +0100

    added main theme

--- front/src/index.js
+++ front/src/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import Theme from './Theme';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Theme />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById('theme')
 );
