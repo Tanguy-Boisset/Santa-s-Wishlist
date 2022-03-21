a1e9ff14221fcb6fd035a14331ab11ca8ec43d36
Tanguy Boisset <tanguy.boisset@gmail.com>
2022-03-22 00:02:58 +0100

    fixed typo

--- front/src/Home/Home.js
+++ front/src/Home/Home.js
@@ -3,8 +3,8 @@ import './Home.css';
 function Home() {
     return (
         <div>
-        <div class="center">
-            <div class="text-center">
+        <div className="center">
+            <div className="text-center">
                 <h1>Santa's Wishlist</h1>
                 <h3>Create and share your Christmas wishlists with your friends !</h3>
 			    <a href="#" className="button">Create an account</a>
