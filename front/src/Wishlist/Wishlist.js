1288f50c157ca3734c2df913d4d6d2d32b2c896b
Tanguy Boisset <tanguy.boisset@gmail.com>
2022-03-30 11:56:15 +0200

    first step wishlist

--- front/src/Wishlist/Wishlist.js
+++ front/src/Wishlist/Wishlist.js
@@ -1,12 +1,49 @@
 import './Wishlist.css';
 
+// Data exported from Database
+let cadeau1 = {
+    'name' : 'Nintendo Switch',
+    'description' : 'En tant que grand gamer, j\'ai besoin d\'une Switch',
+    'link' : 'https://www.amazon.fr/Nintendo-Switch-avec-paire-Rouge/dp/B07WKNQ8JT/',
+    'price' : 270
+};
+
+let cadeau2 = {
+    'name' : 'Nintendo Switch',
+    'description' : 'En tant que grand gamer, j\'ai besoin d\'une Switch',
+    'link' : 'https://www.amazon.fr/Nintendo-Switch-avec-paire-Rouge/dp/B07WKNQ8JT/',
+    'price' : 270
+};
+
+let cadeau3 = {
+    'name' : 'Nintendo Switch',
+    'description' : 'En tant que grand gamer, j\'ai besoin d\'une Switch',
+    'link' : 'https://www.amazon.fr/Nintendo-Switch-avec-paire-Rouge/dp/B07WKNQ8JT/',
+    'price' : 270
+};
+
+let myWishlist = {
+    'name': 'La Wishlist de Tanguy :D',
+    'date': '28-01-2022',
+    'description': 'Voilà tous les cadeaux que j\'adoooorerais avoir pour Noël !! :D',
+    'cadeaux': [cadeau1,cadeau2,cadeau3]
+};
+
+
 function Wishlist() {
     return (
         <div>
         <div className="center-top">
             <div className="text-center">
-                <h1>My Wishlist</h1>
-                <h3>Damn</h3>
+                <h1>{myWishlist.name}</h1>
+                <h3>{myWishlist.description}</h3>
+                <h4>Créée le {myWishlist.date}</h4>
+                <ul>
+                    {myWishlist.cadeaux.map(function(cadeau){
+            		    return <li>{cadeau.name}</li>;
+          		})}
+                </ul>
+
 	        </div>
         </div>
         </div>
