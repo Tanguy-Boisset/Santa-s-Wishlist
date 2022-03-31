e49b3c3c06b9d85001458d8a8abfd489860d67a9
VangelisHoareau <vangelis_hoareau@yahoo.com>
2022-03-31 13:40:57 +0200

    changement visuel quand les champs passwords ne correspondent pas

--- front/src/Signup/lib.js
+++ front/src/Signup/lib.js
@@ -1,22 +1,62 @@
-function checkInfo(){
+function submitSignup(){
     let nodePassword = document.getElementById("password");
     let nodePasswordCheck = document.getElementById("passwordCheck");
     
     let nodeError = document.getElementById("error")
     
-    console.log(nodePassword);
-    console.log(nodePasswordCheck);
+    // console.log(nodePassword);
+    // console.log(nodePasswordCheck);
     
     let password = nodePassword.value;
     let passwordCheck = nodePasswordCheck.value;
 
     if (password==passwordCheck){
-        document.forms["formSignup"].submit();
-        window.location.replace("/loginBackend");
+        let nodeForm = document.getElementsByTagName("input")
+        // document.forms["formSignup"].submit();
+        // fetch("127.0.0.1:5000/signup", requestOption);
+        const requestOptions = {
+            method: 'POST',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+                },
+            body: JSON.stringify({
+                                name: nodeForm[0].value,
+                                surname: nodeForm[1].value,
+                                pseudo: nodeForm[2].value,
+                                password: nodeForm[3].value,
+                                })
+            }
+            fetch("http://localhost:3000/signup", requestOptions)
     }
     else{
-        nodeError.innerHTML="Le mot de passe doit être identique dans les deux champs !"
+        nodePassword.style.border="2px red solid";
+        nodePassword.style.borderRadius="3px";
+        nodePasswordCheck.style.border="2px red solid";
+        nodePasswordCheck.style.borderRadius="3px";
+        nodeError.innerHTML="Fields are not matching !"
     }
 }
 
-export default checkInfo;
+// function submitSignup(e){
+//     // console.log("non")
+//     const requestOption = {
+//     headers: {
+//         'Accept': 'application/json',
+//         'Content-Type': 'application/json'
+//         },
+//     body: JSON.stringify({
+//                         // name: this.state.name, 
+//                         // surname: this.state.surname,
+//                         // pseudo: this.state.pseudo,
+//                         // password: this.state.password
+//                             test:"test"
+//                         })
+                        
+//                     }
+//     fetch("http://127.0.0.1:3000/good", requestOption)
+    
+// }
+
+export default submitSignup;
+// export {submitSignup}
