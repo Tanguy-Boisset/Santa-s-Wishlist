b73940fcdca1bb15390e9366c522f303b7362c1e
VangelisHoareau <vangelis_hoareau@yahoo.com>
2022-04-01 11:50:16 +0200

    Ajout de la gestion de la reponse serveur + des gestions d'erreurs pseudo et password

--- front/src/Signup/lib.js
+++ front/src/Signup/lib.js
@@ -1,8 +1,39 @@
+async function grantToken(element, options){
+    let respRaw = await (await fetch('http://127.0.0.1:5000/signup',options)).text();
+    let respJson = JSON.parse(respRaw);
+    // if respJson.token
+    if (respJson.access_token == undefined){
+        if (respJson.msg == 'Pseudo is already in Database'){
+            document.getElementById("pseudo").style.border="2px red solid";
+            document.getElementById("pseudo").style.borderRadius="3px";
+            document.getElementById("password").style.border="";
+            document.getElementById("password").style.borderRadius="";
+            document.getElementById("passwordCheck").style.border="";
+            document.getElementById("passwordCheck").style.borderRadius="";
+
+        }
+        else{
+            document.getElementById("password").style.border="2px red solid";
+            document.getElementById("password").style.borderRadius="3px";
+            document.getElementById("passwordCheck").style.border="2px red solid";
+            document.getElementById("passwordCheck").style.borderRadius="3px";
+            document.getElementById("pseudo").style.border="";
+            document.getElementById("pseudo").style.borderRadius="";
+        }
+        element.innerHTML=respJson.msg;
+    }
+    else{
+        // let token = respJson.access_token;
+
+    }
+        console.log(respRaw);
+}
+
 function submitSignup(){
     let nodePassword = document.getElementById("password");
     let nodePasswordCheck = document.getElementById("passwordCheck");
-    
-    let nodeError = document.getElementById("error")
+    let nodeError = document.getElementById("error");
+    let nodePseudo = document.getElementById("pseudo");
     
     // console.log(nodePassword);
     // console.log(nodePasswordCheck);
@@ -28,15 +59,18 @@ function submitSignup(){
                                 })
             }
             
-            fetch('http://127.0.0.1:5000/signup',requestOptions)
-                .then(response => {
-                    console.log(response.statusText);
-                    return response.text();
-                })
-                .then(data => console.log(data));
-            
+            // fetch('http://127.0.0.1:5000/signup',requestOptions)
+            //     .then(response => {
+            //         console.log(response.statusText);
+            //         return response.text();
+            //     })
+            //     .then(data => console.log(data));
+
+            grantToken(nodeError, requestOptions); 
     }
     else{
+        nodePseudo.style.border="";
+        nodePseudo.style.borderRadius="";
         nodePassword.style.border="2px red solid";
         nodePassword.style.borderRadius="3px";
         nodePasswordCheck.style.border="2px red solid";
