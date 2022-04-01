0f6f05255d9b1bec240fe4c018c0815d8dacb865
Tanguy Boisset <tanguy.boisset@gmail.com>
2022-04-01 13:49:34 +0200

    lil fix for Link

--- front/src/Wishlist-List/WishlistElem/WishlistElem.js
+++ front/src/Wishlist-List/WishlistElem/WishlistElem.js
@@ -3,13 +3,14 @@ import './WishlistElem.css';
 
 function WishlistElem(props) {
     return (
-        <Link to={"../wishlist/" + props.hashed_url} className="wishlistElemLink">
         <div className="wishlistElem">
+            <Link to={"../wishlist/" + props.hashed_url} className="wishlistElemLink">
             <h5 className="wishlistElemName">{props.name}</h5>
             <p className="wishlistElemDate">Créée par {props.name_creator}</p>
             <p className="wishlistElemDesc">{props.description}</p>
+            </Link>
         </div>
-        </Link>
+       
     );
     }
 
