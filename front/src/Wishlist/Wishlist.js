3372944ebc406e0bd35179ff556de8aa744bde92
Tanguy Boisset <tanguy.boisset@gmail.com>
2022-04-01 11:53:36 +0200

    removed test data

--- front/src/Wishlist/Wishlist.js
+++ front/src/Wishlist/Wishlist.js
@@ -2,42 +2,6 @@ import { useState, useEffect } from 'react';
 import './Wishlist.css';
 import Gift, {AddGift} from '../Gift/Gift';
 
-// Data exported from Database
-let cadeau1 = {
-    'id': 0,
-    'name' : 'Un stage',
-    'description' : 'Nolwenn bordel tu fous quoi !?',
-    'link' : 'https://www.amazon.fr/Nintendo-Switch-avec-paire-Rouge/dp/B07WKNQ8JT/',
-    'price' : 5,
-    'chosen': true
-};
-
-let cadeau2 = {
-    'id': 1,
-    'name' : 'Une validation en OS',
-    'description' : 'Stp, pas les rattrapages',
-    'link' : 'https://www.amazon.fr/Nintendo-Switch-avec-paire-Rouge/dp/B07WKNQ8JT/',
-    'price' : 0,
-    'chosen': true
-};
-
-let cadeau3 = {
-    'id': 2,
-    'name' : 'Nintendo Switch',
-    'description' : 'En tant que grand gamer, j\'ai besoin d\'une Switch',
-    'link' : 'https://www.amazon.fr/Nintendo-Switch-avec-paire-Rouge/dp/B07WKNQ8JT/',
-    'price' : 270,
-    'chosen': true
-};
-
-let myWishlist = {
-    'id': 0,
-    'name': 'La Wishlist de Tanguy :D',
-    'description': 'Voilà tous les cadeaux que j\'adoooorerais avoir pour Noël !! :D',
-    'cadeaux': [cadeau1,cadeau2,cadeau3,cadeau3,cadeau3]
-};
-
-
 function Wishlist(id_wishlist=1) {
     let [my_wishlist, setDataWishlist] = useState([]);
 
