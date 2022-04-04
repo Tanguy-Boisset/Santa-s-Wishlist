4ba779020b9b25ff6f3a18d621c637ed5823783e
VangelisHoareau <vangelis_hoareau@yahoo.com>
2022-04-04 16:50:26 +0200

    Navbar dynamique avec la connexion

--- front/src/Navbar/Navbar.js
+++ front/src/Navbar/Navbar.js
@@ -4,6 +4,7 @@ import './Navbar.css';
 import logout from "./libNavbar.js";
 
 function Navbar() {
+  if (localStorage.getItem("santaToken") === null) {
     return (
         <div className="nav">
         <ul>
@@ -17,17 +18,35 @@ function Navbar() {
             <Link to="/wishlist-list">Friends' Wishlists</Link>
           </li>
           <li>
-            <Link to="/login">Log in</Link>
-          </li>
-          <li>
-            <a onClick={logout}><u>Log out</u></a>
-          </li>
-          <li>
             <Link to="/signup">Sign up</Link>
           </li>
+          <li>
+          <Link to="/login">Log in</Link>
+        </li>
         </ul>
         </div>
     );
-    }
+  }
+  else{
+    return (
+      <div className="nav">
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/wishlist">My Wishlist</Link>
+        </li>
+        <li>
+          <Link to="/wishlist-list">Friends' Wishlists</Link>
+        </li>
+        <li>
+          <a onClick={logout}><u>Log out</u></a>
+        </li>
+      </ul>
+      </div>  
+    );
+  }
+}
 
 export default Navbar;
