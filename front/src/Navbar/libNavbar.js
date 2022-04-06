e10ac612590d1971e83dcdab90c4022256f50bec
VangelisHoareau <vangelis_hoareau@yahoo.com>
2022-04-06 16:19:34 +0200

    gestion dynamique de navbar avec la connexion plus propre

--- front/src/Navbar/libNavbar.js
+++ front/src/Navbar/libNavbar.js
@@ -1,6 +1,34 @@
+import { Link } from "react-router-dom";
+
+
 function logout(){
     console.log(localStorage.clear());
     window.location.href = "/";
 }
 
+function Connected(){
+    if (localStorage.getItem("santaToken") === null){
+        return(
+            <span>
+                <li>
+                <Link to="/signup">Sign up</Link>
+                </li>
+                <li>
+                    <Link to="/login">Log in</Link>
+                </li>
+            </span>
+        )
+    }
+    else{
+        return(
+            <li>
+                <a onClick={logout}><u>Log out</u></a>
+            </li>
+    //     <div id="connected">connected as : <span id='username'></span></div>
+        )
+            
+    }
+}
+
 export default logout;
+export {Connected};
